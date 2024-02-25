1f00de68114f33254245e97cd9e64c0eb8c93ffd
yodabytz <122388115+yodabytz@users.noreply.github.com>
2024-02-25 15:28:44 -0500

    Update fail2many.sh
    Added in the ability to also check the rotated logfile /var/log/auth.log.1

--- fail2many.sh
+++ fail2many.sh
@@ -16,21 +16,31 @@ ip_exists() {
     fi
 }
 
-while read -r line; do
-    if grep -q 'Ban' <<< "$line"; then
-        ip_address=$(awk '{print $8}' <<< "$line")  # Adjusted to extract the IP address correctly
-        if [ "$ip_address" != "Ban" ]; then
-            offenders["$ip_address"]=$((${offenders["$ip_address"]:-0} + 1))
+# Function to process log files
+process_log_file() {
+    local log_file=$1
+    while read -r line; do
+        if grep -q 'Ban' <<< "$line"; then
+            ip_address=$(awk '{print $8}' <<< "$line")  # Adjusted to extract the IP address correctly
+            if [ "$ip_address" != "Ban" ]; then
+                offenders["$ip_address"]=$((${offenders["$ip_address"]:-0} + 1))
 
-            if [ ${offenders["$ip_address"]} -ge $threshold ]; then
-                if ! ip_exists "$ip_address"; then
-                    echo $ip_address >> /etc/fail2many.txt
-                    ((banned_ips++))
+                if [ ${offenders["$ip_address"]} -ge $threshold ]; then
+                    if ! ip_exists "$ip_address"; then
+                        echo $ip_address >> /etc/fail2many.txt
+                        ((banned_ips++))
+                    fi
                 fi
             fi
         fi
-    fi
-done < /var/log/fail2ban.log
+    done < "$log_file"
+}
+
+# Process the primary log file
+process_log_file "/var/log/fail2ban.log"
+
+# Process the rotated log file
+process_log_file "/var/log/fail2ban.log.1"
 
 if [ $banned_ips -gt 0 ]; then
     echo "Added $banned_ips IPs to /etc/fail2many.txt"
